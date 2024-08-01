69a3dd9e552b418393266da27d50b11a2f3049b4
Sergio Navarrete <sergionvte11@gmail.com>
2024-08-01 12:55:50 -0600

    agregado STATIC_ROOT

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
