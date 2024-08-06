d04add556890cf2b654e9e9f0477336b98183694
Sergio Navarrete <sergionvte11@gmail.com>
2024-08-06 10:08:18 -0600

    configuracion adaptada para fly.io

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,9 +29,10 @@ SECRET_KEY = 'django-insecure-g&3@!qotq-qrh4#f_a2w**m-2$i=)d7a#a0k7)ndisxl0s7jx^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.getenv('DEBUG'))
 
+APP_NAME = os.environ.get('FLY_APP_NAME')
 ALLOWED_HOSTS = [
-    'sergionvte.pythonanywhere.com',
-    '127.0.0.1'
+    '127.0.0.1',
+    f'{APP_NAME}.fly.dev'
 ]
 
 
